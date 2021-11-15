d935891466394d98e49e1cd538b87b9a852b3773
s24575 <sawickigrzegorz66@gmail.com>
2021-11-15 17:45:43 +0100

    Calculator v2

--- 5. OOP/Clock/header.h
+++ 5. OOP/Clock/header.h
@@ -4,52 +4,238 @@
 #include <memory>
 #include <queue>
 #include <stack>
+#include <stdexcept>
 #include <iostream>
 #include <string>
 
 struct Element {
 	virtual void evaluate(std::stack<double>&) const = 0;
-	virtual ~Element();
+	virtual ~Element() {}
 };
-/*
+
 struct Literal : Element {
 	double const value{ 0.0 };
 
-	void evaluate(std::stack<double>&) const override;
-
-	Literal(double const);
-	};
+	void evaluate(std::stack<double>& stack) const override
+	{
+		stack.push(value);
+	}
+	Literal(double const x) : value(x) {}
+};
 
 struct Print : Element {
-	void evaluate(std::stack<double>&) const override;
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.empty()) {
+			throw std::logic_error{ "empty stack" };
+		}
+		auto element = std::move(stack.top());
+		std::cout << element << '\n';
+	}
 };
 
 struct Addition : Element {
-	void evaluate(std::stack<double>&) const override;
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for + operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(a + b);
+	}
 };
-*/
+
+struct Subtraction : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for - operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(a - b);
+	}
+};
+
+struct Multiplication : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for * operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(a * b);
+	}
+};
+
+struct Division : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for / operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(a / b);
+	}
+};
+
+struct Division_int : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for // operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(int(a / b));
+	}
+};
+
+struct Modulo : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for % operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(int(a) % int(b));
+	}
+};
+
+struct Exponentiation : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 2) {
+			throw std::logic_error{ "invadid amount of numbers for ** operation" };
+		}
+		double b = stack.top();
+		stack.pop();
+		double a = stack.top();
+		stack.pop();
+		stack.push(pow(a, b));
+	}
+};
+
+struct Square_root : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 1) {
+			throw std::logic_error{ "invadid amount of numbers for sqrt operation" };
+		}
+		double a = stack.top();
+		stack.pop();
+		stack.push(sqrt(a));
+	}
+};
+
+struct Factorial : Element {
+	void evaluate(std::stack<double>& stack) const override
+	{
+		if (stack.size() != 1) {
+			throw std::logic_error{ "invadid amount of numbers for ! operation" };
+		}
+		double a = stack.top();
+		stack.pop();
+		int fact = 1;
+		for (int i = 1; i <= a; i++)
+			fact = fact * i;
+		stack.push(fact);
+	}
+};
+
 struct Calculator {
-	std::vector<std::string> stack1;
 	std::stack<double> stack;
 	std::queue<std::unique_ptr<Element>> ops;
 
-	// Schedule operation for evaluation.
-
-/*
-	void push(std::unique_ptr<Element>);
-
 	// Convert string to element and push it on the queue of operations to perform.
-
-	void push(std::string);
+	void push(std::string x)
+	{
+		if (x == "p")
+		{
+			std::unique_ptr<Element> print = std::make_unique<Print>();
+			ops.push(std::move(print));
+		}
+		else if (x == "+")
+		{
+			std::unique_ptr<Element> plus = std::make_unique<Addition>();
+			ops.push(std::move(plus));
+		}
+		else if (x == "-")
+		{
+			std::unique_ptr<Element> minus = std::make_unique<Subtraction>();
+			ops.push(std::move(minus));
+		}
+		else if (x == "*")
+		{
+			std::unique_ptr<Element> times = std::make_unique<Multiplication>();
+			ops.push(std::move(times));
+		}
+		else if (x == "/")
+		{
+			std::unique_ptr<Element> divided = std::make_unique<Division>();
+			ops.push(std::move(divided));
+		}
+		else if (x == "//")
+		{
+			std::unique_ptr<Element> divided_int = std::make_unique<Division_int>();
+			ops.push(std::move(divided_int));
+		}
+		else if (x == "%")
+		{
+			std::unique_ptr<Element> modulo = std::make_unique<Modulo>();
+			ops.push(std::move(modulo));
+		}
+		else if (x == "**")
+		{
+			std::unique_ptr<Element> power = std::make_unique<Exponentiation>();
+			ops.push(std::move(power));
+		}
+		else if (x == "sqrt")
+		{
+			std::unique_ptr<Element> sqrt = std::make_unique<Square_root>();
+			ops.push(std::move(sqrt));
+		}
+		else if (x == "!")
+		{
+			std::unique_ptr<Element> factorial = std::make_unique<Factorial>();
+			ops.push(std::move(factorial));
+		}
+		else
+		{
+			std::unique_ptr<Element> num = std::make_unique<Literal>(std::stod(x));
+			ops.push(std::move(num));
+		}
+	}
 
 	// Evaluate scheduled operations.
+	void evaluate()
+	{
+		while (!ops.empty())
+		{
+			std::unique_ptr<Element>& x = ops.front();
+			x->evaluate(stack);
+			ops.pop();
+		}
+	}
 
-	void evaluate();
-*/
 	Calculator() = default;
-	Calculator(std::stack<double>);
-	Calculator(std::vector<std::string>);
-
+	//Calculator(std::stack<double>) {}
 };
 
 #endif
--- 5. OOP/Clock/main.cpp
+++ 5. OOP/Clock/main.cpp
@@ -1,49 +1,24 @@
 #include "header.h"
 #include <algorithm>
 #include <iterator>
-#include <stdexcept>
 #include <vector>
 #include <cmath>
 
 std::vector<std::string> make_args(int argc, char* argv[])
 {
-    auto args = std::vector<std::string>{};
-    std::copy_n(argv, argc, std::back_inserter(args));
-    return args;
+	auto args = std::vector<std::string>{};
+	std::copy_n(argv, argc, std::back_inserter(args));
+	return args;
 }
 
 int main(int argc, char* argv[])
 {
-    auto const args = make_args(argc - 1, argv + 1);
-	Calculator calc();
+	auto const args = make_args(argc - 1, argv + 1);
+	std::unique_ptr<Calculator> calculator = std::make_unique<Calculator>();
 	for (auto const each : args)
-		std::cout << each << '\n';
-	for (auto const each : args) {
-		if (each == "p")
-			std::cout << "print" << '\n';
-		else if (each == "+")
-			std::cout << "plus" << '\n';
-		else {
-			//stack.push(std::stod(each));
-			std::cout << "stack.push " << each << '\n';
-			//calc.stack = (std::stod(each));
-			}
-		}
-	//calc.logic(calc);
-    return 0;
+	{
+		calculator->push(each);
+	}
+	calculator->evaluate();
+	return 0;
 }
-
-/*
-input: 2 2 + p
-["2","2","+","p"]
-2 + 2 print
-output: 4
-
-Calculator calc(args);
-calc.print(calc);
-1. make a calculator object (std::stack<double>) so the stack = 2 2 + p
-2. stack.push stack.push Addition::evaluate Print::evaluate
-2. make a std::queue (pointers to Element class) stack.pop stack.pop evaluate print
-3. schedule evaluation std::stack<double> using an Element subclass?
-4. calc.push(stack) 
-*/
